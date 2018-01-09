055c09094ef9a72b2f00a8bf081b0042c709b9c3
kensupermen <votantho93@gmail.com>
2018-01-10 00:28:54 +0700

    Add reviews

---
+++ app/javascript/angular/app/shared/layout/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'layout-header',
+  template: require('./header.component.html')
+})
+
+export class HeaderComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
