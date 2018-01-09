a88b992a4b39340bbf613362f6a7baf49d23ac97
kensupermen <votantho93@gmail.com>
2018-01-09 23:30:32 +0700

    :collision: add angular template config :collision:

--- app/javascript/angular/app/app.component.ts
+++ app/javascript/angular/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'hello-angular',
-  template: `<h1>Hello {{name}}</h1>`
+  selector: 'app-root',
+  template: require('./app.component.html')
 })
 export class AppComponent {
   name = 'Angular!';
