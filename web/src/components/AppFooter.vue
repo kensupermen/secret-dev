6e961b9bca57014d55999f912834344ec1905948
kensupermen <votantho93@gmail.com>
2018-01-20 22:05:56 +0700

    Add footer and header

---
+++ web/src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer">
+    This is a footer
+    <i class="fa fa-github fa-2x" aria-hidden="true"></i>
+    <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      msg: 'Footer',
+    };
+  },
+};
+</script>
---
+++ web/src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+    <div class="container">
+      <a class="navbar-brand" href="#">Secret Of Dev</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      msg: 'Header',
+    };
+  },
+};
+</script>
