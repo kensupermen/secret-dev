767d032eabddd3af4119cc0d5bc4bff6563b3b90
kensupermen <votantho93@gmail.com>
2018-01-24 21:06:04 +0700

    refactor get all companies

--- web/src/components/Review.vue
+++ web/src/components/Review.vue
@@ -3,7 +3,7 @@
     <input type="text" class="form-control" placeholder="Tim lang cua tui"/>
     <div>
       <h2>Companies</h2>
-      {{ allCompanies }}
+      {{ companies }}
     </div>
   </div>
 </template>
@@ -14,13 +14,22 @@ import axios from 'axios'
 export default {
   name: 'Review',
 
-  computed: {
-    allCompanies() {
+  data() {
+    return {
+      companies: []
+    }
+  },
+
+  mounted () {
+    this.fetchCompanies()
+  },
+  methods: {
+    fetchCompanies() {
       axios.get('http://localhost:3000/api/v1/companies')
         .then((response) => {
-          this.$store.state.companies = response.data
+          // this.$store.state.companies = response.data
+          this.companies = response.data
         })
-      return this.$store.state.companies
     }
   }
 }
